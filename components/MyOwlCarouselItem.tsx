ec4e5f8e8ee9cf607142b0607d3958f536a198ad
vankata <ivantsoninski@gmail.com>
2023-05-10 18:27:20 +0300

    use img instead of Image on homepage carousel

--- components/MyOwlCarouselItem.tsx
+++ components/MyOwlCarouselItem.tsx
@@ -6,7 +6,7 @@ export default function MyOwlCarouselItem(props) {
     return (
         <div /*id="myOwlCarousel"*/ className='item owlCarouselItem'>
             <Link href={"CollectionDetails?address=" + props?.address + "&chain=" + props?.chain} style={{textDecoration: "none"}}>
-                <Image alt={""} width={307} height={307} src={props.image} />
+                <img alt={""} width={307} height={307} src={props.image} />
                 <h4 className="myOwlCarouselH">{props.name}</h4>
             </Link>
         </div>
