867d358b197fa260071e91943ad0d28fb8c2fd7e
RaduMirceaAndrei00 <mirad28@yahoo.com>
2023-07-07 18:27:51 +0300

    one more fix

--- pages/Profile.tsx
+++ pages/Profile.tsx
@@ -9,7 +9,7 @@ import ProfilePublicKeys from '../components/ProfilePublicKeys';
 import { KEYWORDS } from '../pages/_app';
 import { useRouter } from 'next/router';
 import { get_collections, get_nfts, Collection, NFT, database_awake } from '../lib/indexer_api';
-import { useAccount, useDisconnect, useContract } from "wagmi";
+import { useAccount, useDisconnect } from "wagmi";
 import { useNetworkContext } from '../contexts/networkContext';
 
 export default function Profile(props) {
